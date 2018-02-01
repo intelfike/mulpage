0bbedcedf8b7cdeda9049de10f94c4ee492a275f
intelfike <intelfike@gmail.com>
2018-02-01 16:39:22 +0900

    ver 1.6: タイトルを適切に設定するプログラムを追加

--- contents/isear/page/PageCall.go
+++ contents/isear/page/PageCall.go
@@ -28,40 +28,10 @@ func (c *Content) Define(con *types.Package) {
 	con.Before = func(tpl *types.TplData, info types.PageInfo) *types.Redirect {
 		tpl.Assign("Packages", con.ChildrenArray())
 		tpl.Assign("Package", con.Children[info.Package])
+		if v, ok := con.Children[info.Package].Articles[info.Method]; ok {
+			tpl.Assign("Title", v.Name)
+		}
 		tpl.Assign("Info", info)
 		return nil
 	}
-
-	// type PA struct {
-	// 	Package string
-	// 	Ins     types.PackageIfc
-	// 	Text    string
-	// } // Package Assign
-
-	// PArr := []PA{
-	// 	PA{"top", &top.Package{}, ""},
-	// 	PA{"usage", &usage.Package{}, ""},
-	// 	PA{"support", &support.Package{}, ""},
-	// 	PA{"install", &install.Package{}, ""},
-	// } // Package Array
-	// PList := map[string]types.PackageIfc{}
-
-	// for _, v := range PArr {
-	// 	PList[v.Package] = v.Ins
-	// }
-
-	// // パッケージのリストを定義
-	// content.Init("isear", PList)
-
-	// for n, v := range PArr {
-	// 	name := content.Packages[v.Package].Name
-	// 	PArr[n].Text = name
-	// }
-
-	// content.Before = func(tpl *types.TplData, info types.PageInfo) *types.Redirect {
-	// 	tpl.Assign("Packages", PArr)
-	// 	tpl.Assign("Package", content.Packages[info.Package])
-	// 	tpl.Assign("Info", info)
-	// 	return nil
-	// }
 }
